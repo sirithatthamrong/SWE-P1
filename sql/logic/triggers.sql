274daac57b9d8bf2620e8e709c0640ad08ebbbf3
lingg <kanladaporn.sir@gmail.com>
2025-01-21 14:10:43 +0700

    Auto archive old reservations, Add action(active, canceled, archived) to RoomReservations table and drop RoomReservationLogs + updating/inserting instead of deleting

--- sql/logic/triggers.sql
+++ sql/logic/triggers.sql
@@ -1,23 +1,22 @@
 DROP TRIGGER IF EXISTS check_booking_overlap ON RoomReservations;
 CREATE TRIGGER check_booking_overlap
-BEFORE INSERT ON RoomReservations
-FOR EACH ROW
+    BEFORE INSERT
+    ON RoomReservations
+    FOR EACH ROW
 EXECUTE FUNCTION prevent_double_booking();
 
 
 DROP TRIGGER IF EXISTS tasks_validation_trigger ON Tasks;
 CREATE TRIGGER tasks_validation_trigger
-BEFORE INSERT OR UPDATE
-ON Tasks
-FOR EACH ROW
+    BEFORE INSERT OR UPDATE
+    ON Tasks
+    FOR EACH ROW
 EXECUTE FUNCTION tasks_validation();
 
-DROP TRIGGER IF EXISTS track_cancelations ON RoomReservations;
-CREATE TRIGGER track_cancelations
-BEFORE DELETE ON RoomReservations
-FOR EACH ROW EXECUTE FUNCTION log_cancelation();
-
-DROP TRIGGER IF EXISTS auto_delete_old_reservations ON RoomReservations;
-CREATE TRIGGER auto_delete_old_reservations
-AFTER INSERT OR UPDATE ON RoomReservations
-EXECUTE FUNCTION delete_old_reservations();
+DROP TRIGGER IF EXISTS auto_archive_old_reservations ON RoomReservations;
+CREATE TRIGGER auto_archive_old_reservations
+    AFTER INSERT OR UPDATE
+    ON RoomReservations
+    FOR EACH ROW
+    WHEN (NEW.date < CURRENT_DATE) -- Runs only for past reservations
+EXECUTE FUNCTION archive_old_reservations();
